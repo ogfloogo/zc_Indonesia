d95b3534231e370ac09265f6e9050cbf931deb3b
hzx <hzx@gmail.com>
2023-10-18 15:42:49 +0800

    .

---
+++ application/api/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//配置文件
+return [
+    'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
+    'default_return_type'    => 'json',
+    'image_url' => "https://api.alphafund.in",
+    'invite_url' => "https://www.alphafund.in"
+];
