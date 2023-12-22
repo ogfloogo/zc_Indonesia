f0f5a940ee7555f7907143fd10746fd615fc64f8
hzx <hzx@gmail.com>
2023-12-22 14:48:08 +0800

    1

--- application/admin/controller/activity/Turntabletimes.php
+++ application/admin/controller/activity/Turntabletimes.php
@@ -3,6 +3,9 @@
 namespace app\admin\controller\activity;
 
 use app\common\controller\Backend;
+use think\Db;
+use think\exception\PDOException;
+use think\exception\ValidateException;
 
 /**
  * 用户抽奖次数
@@ -33,5 +36,42 @@ class Turntabletimes extends Backend
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
+    public function add()
+    {
+        if (false === $this->request->isPost()) {
+            return $this->view->fetch();
+        }
+        $params = $this->request->post('row/a');
+        if (empty($params)) {
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        $params = $this->preExcludeFields($params);
 
+        if ($this->dataLimit && $this->dataLimitFieldAutoFill) {
+            $params[$this->dataLimitField] = $this->auth->id;
+        }
+        $exist = (new \app\admin\model\activity\Turntabletimes())->where(['a_id'=>$params['a_id'],'user_id'=>$params['user_id']])->find();
+        if($exist){
+            $this->error('记录已存在，需要修改次数请直接编辑');
+        }
+        $result = false;
+        Db::startTrans();
+        try {
+            //是否采用模型验证
+            if ($this->modelValidate) {
+                $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
+                $this->model->validateFailException()->validate($validate);
+            }
+            $result = $this->model->allowField(true)->save($params);
+            Db::commit();
+        } catch (ValidateException|PDOException|Exception $e) {
+            Db::rollback();
+            $this->error($e->getMessage());
+        }
+        if ($result === false) {
+            $this->error(__('No rows were inserted'));
+        }
+        $this->success();
+    }
 }
