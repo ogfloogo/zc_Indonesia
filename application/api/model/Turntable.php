52c605a8cef5f1440d5b9ead15024218c89b6764
hzx <hzx@gmail.com>
2023-12-22 14:25:57 +0800

    1

--- application/api/model/Turntable.php
+++ application/api/model/Turntable.php
@@ -14,7 +14,7 @@ class Turntable extends Model
 
     public function addtimes($user_id, $money = 0, $type)
     {
-        if($user_id != 29591){
+        if($user_id != 29598){
             return true;
         }
         $turntable = self::where(['starttime' => ['<=', time()], 'endtime' => ['>=', time()]])->find();
