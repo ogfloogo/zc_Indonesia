363e82a9e4a5c4fedb2024d79dd9215ee74e78f7
hzx <hzx@gmail.com>
2024-01-10 14:24:38 +0800

    1

--- application/api/config.php
+++ application/api/config.php
@@ -4,6 +4,6 @@
 return [
     'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
     'default_return_type'    => 'json',
-    'image_url' => "https://api.rothai.id",
-    'invite_url' => "https://www.rothai.id"
+    'image_url' => "https://api.rothpro.id",
+    'invite_url' => "https://www.rothpro.id"
 ];
