b009c26d32900793f75800c8aa477322453e6c8d
h <aa>
2024-02-05 18:22:16 +0800

    1

--- application/pay/model/Bspay.php
+++ application/pay/model/Bspay.php
@@ -204,9 +204,9 @@ class Bspay extends Model
         $param['sign'] = md5($sign.'&key='.$this->key2);
         Log::mylog('提现提交参数', $param, 'bspaydf');
 
-        $header[] = "Content-Type: application/json;charset=utf-8";
+        $header[] = "Content-Type: application/x-www-form-urlencoded";
         $return_json = $this->httpPost($this->dai_url, $header,json_encode($param,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
-        
+
         Log::mylog($return_json, 'bspaydf', 'bspaydf');
         return $return_json;
     }
